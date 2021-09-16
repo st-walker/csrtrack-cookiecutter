f87e26c6bde63561a1eb1f15185509fb0afd176a
stwalker <stuart.walker@desy.de>
2021-09-16 10:34:55 +0200

    missing hook stuff for esase

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,18 +10,25 @@ PROJECT_DIRECTORY = Path(os.path.realpath(os.path.curdir))
 
 
 FILES = {"zeuthen": PROJECT_DIRECTORY / Path("zeuthen-csrtrk.in"),
-         "bc": PROJECT_DIRECTORY / Path("bc-csrtrk.in")}
+         "bc": PROJECT_DIRECTORY / Path("bc-csrtrk.in")
+         "esase": PROJECT_DIRECTORY / Path("esase-csrtrk.in")
+         }
 
 TARGET = PROJECT_DIRECTORY / "csrtrk.in"
 
 if __name__ == '__main__':
     if '{{ cookiecutter.model_type == "zeuthen" }}':
         FILES["bc"].unlink()
+        FILES["esase"].unlink()
         FILES["zeuthen"].rename(TARGET)
-
     elif '{{ cookiecutter.model_type == "bc" }}':
         FILES["zeuthen"].unlink()
+        FILES["esase"].unlink()
         FILES["bc"].rename(TARGET)
+    elif '{{ cookiecutter.model_type == "esase" }}':
+        FILES["zeuthen"].unlink()
+        FILES["bc"].unlink()
+        FILES["esase"].rename(TARGET)
     else:
         sys.exit(1)
 
